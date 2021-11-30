6b3e4066159c788bc7c129277b2f05600f1c08fe
Pavlo Tsyhanovskyi <61557417+ptsyhanovskyi@users.noreply.github.com>
2021-11-30 22:05:37 +0200

    Create HelloWorldTest.php

---
+++ phpunit/test/HelloWorldTest.php
@@ -0,0 +1,16 @@
+<?php
+
+class HelloWorldTest extends \PHPUnit_Framework_TestCase {
+
+	public function testGreeting() {
+
+		$greeting = "Hello World";
+		$requiredGreeting = "Hello World";
+
+		$this->assertEquals($greeting, $requiredGreeting);
+
+	}
+
+}
+
+?>
